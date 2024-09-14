2d949168e1de8908ee40a6a51b2e0ec8bd9857d7
carry0987 <carry0987@users.noreply.github.com>
2024-09-14 20:43:58 +0800

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import Paginator from './component/paginator';
+import { html } from './module/utils/html';
+import { h } from 'preact';
+import { Config } from './component/config';
+
+export {
+    Paginator,
+    html,
+    Config,
+    h,
+};
