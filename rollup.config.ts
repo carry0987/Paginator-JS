1513bb3fcf9d978289edc6f76f1f28ed8b1c98ae
carry0987 <carry0987@users.noreply.github.com>
2024-09-14 20:41:57 +0800

    Add exports to package.json

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,22 +1,24 @@
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import replace from '@rollup/plugin-replace';
-import resolve from '@rollup/plugin-node-resolve';
+import tsConfigPaths from 'rollup-plugin-tsconfig-paths';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import { dts } from 'rollup-plugin-dts';
 import postcss from 'rollup-plugin-postcss';
 import del from 'rollup-plugin-delete';
 import { createRequire } from 'module';
-const pkg = createRequire(import.meta.url)('./package.json');
 
+const pkg = createRequire(import.meta.url)('./package.json');
 const isProduction = process.env.BUILD === 'production';
+const sourceFile = 'src/index.ts';
 
 const jsConfig = {
-    input: 'src/paginator.ts',
+    input: sourceFile,
     output: [
         {
-            file: pkg.main,
+            file: pkg.exports['.']['umd'],
             format: 'umd',
-            name: 'Paginator',
+            name: 'paginatorjs',
             plugins: isProduction ? [terser()] : []
         }
     ],
@@ -27,7 +29,8 @@ const jsConfig = {
             sourceMap: false
         }),
         typescript(),
-        resolve(),
+        tsConfigPaths(),
+        nodeResolve(),
         replace({
             preventAssignment: true,
             __version__: pkg.version
@@ -36,20 +39,22 @@ const jsConfig = {
 };
 
 const esConfig = {
-    input: 'src/paginator.ts',
+    input: sourceFile,
     output: [
         {
-            file: pkg.module,
+            file: pkg.exports['.']['import'],
             format: 'es'
         }
     ],
     plugins: [
         postcss({
+            inject: false,
             extract: false,
             sourceMap: false
         }),
         typescript(),
-        resolve(),
+        tsConfigPaths(),
+        nodeResolve(),
         replace({
             preventAssignment: true,
             __version__: pkg.version
@@ -58,15 +63,16 @@ const esConfig = {
 };
 
 const dtsConfig = {
-    input: 'dist/paginator.d.ts',
+    input: sourceFile,
     output: {
-        file: pkg.types,
+        file: pkg.exports['.']['types'],
         format: 'es'
     },
     external: [/\.css$/u],
     plugins: [
+        tsConfigPaths(),
         dts(),
-        del({ hook: 'buildEnd', targets: ['!dist/index.js', 'dist/*.d.ts', 'dist/interface', 'dist/module', 'dist/type'] })
+        del({ hook: 'buildEnd', targets: 'dist/dts' })
     ]
 };
 
