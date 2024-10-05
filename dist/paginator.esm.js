416390e4772527598bec247561b60c988dd0f5cf
carry0987 <carry0987@users.noreply.github.com>
2024-10-05 14:17:29 +0800

    Add dark mode

--- dist/paginator.esm.js
+++ dist/paginator.esm.js
@@ -2223,7 +2223,7 @@ class EventEmitter {
 }
 
 class Paginator extends EventEmitter {
-    static version = '2.2.0';
+    static version = '2.2.1';
     config;
     constructor(config) {
         super();
