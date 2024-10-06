48e6010d9dc3210a7a9a660b21e519cfde298109
carry0987 <carry0987@users.noreply.github.com>
2024-10-06 16:44:52 +0800

    Add Hook class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import Paginator from '@/component/paginator';
-import { Options } from '@/interface/options';
+import Hook from '@/component/hook';
 import { html } from '@/module/utils/html';
-import { h } from 'preact';
+import { className } from '@/module/utils/className';
+import { Options } from '@/interface/options';
+import { PluginPosition } from '@/type/plugin';
+import { useEffect, useRef, useState } from 'preact/hooks';
+import { h, createElement, Component, createRef } from 'preact';
 
-export { Paginator, Options, html, h };
+export {
+    Paginator,
+    Options,
+    PluginPosition,
+    useEffect,
+    useRef,
+    useState,
+    Hook as Hooks,
+    html,
+    className,
+    h,
+    createElement,
+    Component,
+    createRef
+};
