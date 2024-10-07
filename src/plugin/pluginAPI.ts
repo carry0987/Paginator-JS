5492e87176409988d98a9d8209759dc381d10004
carry0987 <carry0987@users.noreply.github.com>
2024-10-08 06:24:19 +0800

    Import preact hooks into pluginAPI

--- src/plugin/pluginAPI.ts
+++ src/plugin/pluginAPI.ts
@@ -3,6 +3,11 @@ import { useSelector } from '@/module/hook/useSelector';
 import { useConfig } from '@/module/hook/useConfig';
 import { useOption } from '@/module/hook/useOption';
 import { useTranslator } from '@/module/hook/useTranslator';
+import { classJoin, className } from '@/module/utils/className';
+
+// Preact
+import { useSignal } from '@preact/signals';
+import { useEffect } from 'preact/hooks';
 
 class PluginAPI {
     useStore = useStore;
@@ -10,6 +15,11 @@ class PluginAPI {
     useConfig = useConfig;
     useOption = useOption;
     useTranslator = useTranslator;
+    classJoin = classJoin;
+    className = className;
+    // Preact
+    useSignal = useSignal;
+    useEffect = useEffect;
 }
 
 export const pluginAPI = new PluginAPI();
