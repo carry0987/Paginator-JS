e7dd3ccdfcbd3ad1c3194884c82849190ee9e85b
carry0987 <carry0987@users.noreply.github.com>
2024-10-09 00:33:40 +0800

    [Plugin] Add Status & tabularFormat methods

--- src/plugin/pluginUtil.ts
+++ src/plugin/pluginUtil.ts
@@ -1,10 +1,15 @@
 import { PluginRenderer } from './pluginRenderer';
+import { Status } from '@/type/status';
 import Header from '@/component/header';
 import { classJoin, className } from '@/module/utils/className';
 
 class PluginUtil {
+    // Status
+    public Status = Status;
+
     // Header
     public leafColumns = Header.leafColumns;
+    public tabularFormat = Header.tabularFormat;
 
     // Plugin
     public PluginRenderer = PluginRenderer;
