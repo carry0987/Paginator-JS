c6aec26077885fc5c20130df9f6472922b2b4882
carry0987 <carry0987@users.noreply.github.com>
2025-01-10 00:02:19 +0800

    Bump to v2.2.41

--- plugins/table/dist/table.esm.js
+++ plugins/table/dist/table.esm.js
@@ -2,7 +2,7 @@ import { pluginAPI, PluginUtil, PluginPosition } from '@carry0987/paginator';
 
 var l;l={__e:function(n,l,u,t){for(var i,r,o;l=l.__;)if((i=l.__c)&&!i.__)try{if((r=i.constructor)&&null!=r.getDerivedStateFromError&&(i.setState(r.getDerivedStateFromError(n)),o=i.__d),null!=i.componentDidCatch&&(i.componentDidCatch(n,t||{}),o=i.__d),o)return i.__E=i}catch(l){n=l;}throw n}},"function"==typeof Promise?Promise.prototype.then.bind(Promise.resolve()):setTimeout;
 
-var f=0;function u(e,t,n,o,i,u){t||(t={});var a,c,p=t;if("ref"in p)for(c in p={},t)"ref"==c?a=t[c]:p[c]=t[c];var l$1={type:e,props:p,key:n,ref:a,__k:null,__:null,__b:0,__e:null,__c:null,constructor:void 0,__v:--f,__i:-1,__u:0,__source:i,__self:u};if("function"==typeof e&&(a=e.defaultProps))for(c in a)void 0===p[c]&&(p[c]=a[c]);return l.vnode&&l.vnode(l$1),l$1}
+var f=0;function u(e,t,n,o,i,u){t||(t={});var a,c,p=t;if("ref"in p)for(c in p={},t)"ref"==c?a=t[c]:p[c]=t[c];var l$1={type:e,props:p,key:n,ref:a,__k:null,__:null,__b:0,__e:null,__c:null,constructor:undefined,__v:--f,__i:-1,__u:0,__source:i,__self:u};if("function"==typeof e&&(a=e.defaultProps))for(c in a) undefined===p[c]&&(p[c]=a[c]);return l.vnode&&l.vnode(l$1),l$1}
 
 function TD(props) {
     const config = pluginAPI.useConfig();
@@ -149,7 +149,7 @@ const SetTableRef = (tableRef) => (state) => {
 };
 
 function styleInject(css, ref) {
-  if ( ref === void 0 ) ref = {};
+  if ( ref === undefined ) ref = {};
   var insertAt = ref.insertAt;
 
   if (typeof document === 'undefined') { return; }
